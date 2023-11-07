68b342baf89ddfac1bf2f963891c1358b51045e1
Anowar Hossain Apurbo <ahosien57@gmail.com>
2023-11-07 23:25:23 +0600

    single blade customize

--- resources/views/frontend/single-notice.blade.php
+++ resources/views/frontend/single-notice.blade.php
@@ -1,113 +1,35 @@
 @extends('frontend.components.layouts')
 @section('content')
 
-<style>
-.wrapper {
-    width: 1100px;
-    margin: 0px auto;
-    box-sizing: border-box;
-    padding: 0px 10px;
-}
-.notice-single-main-wrapper {
-    display: grid;
-    grid-template-columns: 3fr 1fr;
-    column-gap: 20px;
-    margin-top: 50px;
-}
-.notice-left-content-wrapper {
-    width: 100%;
-    overflow: hidden;
-}
-.notice-left-content-wrapper> article {
-    box-sizing: border-box;
-    padding: 20px;
-    background: #fff;
-    border: 1px solid #ddd;
-}
+{{-- single notice area --}}
+<div class="blog-list-area section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-8 col-lg-8 col-md-12">
+          <div class="about-content single-sidebar">
+            <h4>{{$notice->title}}</h4>
+            <p>@php echo html_entity_decode($notice->notice) @endphp</p>
+          </div>
+        </div>
+            <div class="col-xl-4 col-lg-4 col-md-12">
+              <div class="blog-right ">
+              <div class="widget widget-thumb single-sidebar blog-single">
+              <h3>Latest Notice</h3>
+              <ul class="thumb_post">
 
-element.style {
-}
-.notice-left-content-wrapper> article h1.entry-title {
-    margin-bottom: 10px;
-    padding-bottom: 10px;
-    border-bottom: 1px solid #ffeae4;
-    color: #464646;
-    font-size: 28px;
-}
-.entry-content> p {
-    padding-bottom: 20px;
-    font-size: 18px;
-    color: #000;
-}
-.notice-right-sidebar-wrapper {
-    width: 100%;
-}
-.side-widget-title {
-    background: linear-gradient(174deg, #FF9800, #FF5722);
-    color: #fff;
-    box-sizing: border-box;
-    padding: 5px 10px;
-    font-size: 22px;
-}
-.sidebar-widget-content {
-    box-sizing: border-box;
-    padding: 10px;
-    background: #fff;
-    border: 1px solid #ddd;
-    border-top: none;
-}
-.sidebar-widget-content> ul {
-    list-style-type: none;
-    display: grid;
-    grid-template-columns: 1fr;
-    row-gap: 5px;
-}
-
-element.style {
-}
-.sidebar-widget-content> ul li {
-    border-bottom: 1px solid #ddd;
-    padding-bottom: 5px;
-}
-.sidebar-widget-content> ul li a {
-    display: block;
-    text-decoration: none;
-    color: blue;
-    font-size: 14px;
-}
-</style>
-
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <div class="wrapper">
-			<div class="notice-single-main-wrapper">
-				<div class="notice-left-content-wrapper">
-					<article id="post-68" class="post-68 post_notice type-post_notice status-publish hentry">
-						<header class="entry-header">
-							<h1 class="entry-title">{{$notice->title}}</h1>
-						</header>
-
-						<div class="entry-content">
-							<p>@php echo html_entity_decode($notice->notice) @endphp</p>
-						</div>
-					</article>
-				</div>
-				<div class="notice-right-sidebar-wrapper">
-					<div class="sidebar-widget">
-						<div class="side-widget-title">
-							<i class="fa fa-bullhorn"></i><span> Latest Notice</span>
-						</div>
-						<div class="sidebar-widget-content">
-                            <ul>
-                                @foreach ($notices as $item)
-                                <li><i class="fa fa-bullhorn"></i> <a href="{{route('single-notice',$item->id)}}">{{$item->title}}</a></li>
-                                @endforeach
-                            </ul>    
-                        </div>
-					</div>
-				</div>
-			</div>
-		</div>
-    </main>
-</div>
+                    @foreach ($notices as $item)                     
+                    <li>
+                    <img src="{{asset('images/6533c7d6604ee-2023-Oct-Sat-12-45-10.Computer%20Operation.webp')}}" alt="">
+                    <h4><a href="{{route('single-notice',$item->id)}}">{{$item->title}}</a></h4>
+                    <p class="post-date">{{ $item->created_at->format('d-m-Y') }}</p>
+                    </li>
+                    @endforeach
+              </ul>
+              <a href="{{route('notice')}}" class="btn-primary p-1">See More</a>
+              </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
--- resources/views/frontend/single_teacher.blade.php
+++ resources/views/frontend/single_teacher.blade.php
@@ -1,18 +1,45 @@
 @extends('frontend.components.layouts')
 @section('content')
-<div class="container">
-        <div class="row justify-content-md-center">
-                    <div class="card " style="width: 50%; text-align: center;">
-                            <img src="{{asset('images/kuddos.jpg')}}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                              <h5 class="card-title">Ala Uddin Kha</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                              </p>
-                              <a href="#" class=""><i class="fa fa-phone"></i> 01755323215</a>
-                              <a href="#" class=""><i class="fa fa-envelope"></i> sample@gmail.com</a>
-                            </div>
-                          </div>
+<div class="blog-list-area section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-8 col-lg-8 col-md-12">
+        <div class="about-content single-sidebar">
+          <img src="{{asset('images/kuddos.jpg')}}" class="card-img-top" alt="Image" style="width:350px; height:350px; margin:0 auto;">
+          <h4>Abdul Kuddos</h4>
+          <p>SYDTC Training Institute. We are helping people to develop their skills over the last 5 years. </p>
+          
+          <a href="#" class=""><i class="fa fa-phone"></i> 01755323215</a>
+          <a href="#" class=""><i class="fa fa-envelope"></i> sample@gmail.com</a>
         </div>
-</div>
+      </div>
+          <div class="col-xl-4 col-lg-4 col-md-12">
+            <div class="blog-right ">
+            <div class="widget widget-thumb single-sidebar blog-single">
+            <h3>Latest Notice</h3>
+            <ul class="thumb_post">
+            <li>
+            <img src="images/64d1e6ef03e2c-2023-Aug-Tue-06-55-43.For%20web.webp" alt="Full Stack Flutter: Building Dynamic Apps &amp; Games with Firebase, Spring Boot and Flame Engine">
+            <h4><a href="#">Full Stack Flutter: Building Dynamic Apps &amp; Games with Firebase, Spring Boot and Flame Engine Batch No : 1</a></h4>
+            <p class="post-date">15 Nov, 2023</p>
+            </li>
+            <li>
+            <img src="images/650969040a5d3-2023-Sep-Tue-09-25-24.Digital-Marketing.webp" alt="Digital Marketing for Freelancing- Level 4 (NSDA)">
+            <h4><a href="#">Digital Marketing for Freelancing- Level 4 (NSDA) Batch No : 2</a></h4>
+            <p class="post-date">21 Nov, 2023</p>
+            </li>
+            <li>
+            <img src="images/6533c7d6604ee-2023-Oct-Sat-12-45-10.Computer%20Operation.webp" alt="Computer Operation- Level 03 (NSDA)">
+            <h4><a href="#">Computer Operation- Level 03 (NSDA) Batch No : 16</a></h4>
+            <p class="post-date">21 Nov, 2023</p>
+            </li>
+            </li>
+            </ul>
+            <a href="{{route('notice')}}" class="btn-primary p-1">See More</a>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
